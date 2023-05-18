17fc99cab7f72c5411b974afa338954ce30d9543
robiul-romjan <robiulromjan7@gmail.com>
2023-05-18 13:26:07 +0600

    post toy successfully

--- Index.js
+++ Index.js
@@ -25,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toysCollection = client.db("kiddieCorner").collection("toys");
+
+    app.post("/addToys", async(req, res)=> {
+      const newToys = req.body;
+      const result = await toysCollection.insertOne(newToys);
+      res.send(result);
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
